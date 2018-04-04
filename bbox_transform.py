ef3b38da346a24b5d5c443aa28d4ec8754270ba6
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-04-04 17:59:22 +0800

    Add files via upload

---
+++ bbox_transform.py
@@ -0,0 +1,114 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import torch
+
+
+def bbox_transform(ex_rois, gt_rois):
+    ex_widths = ex_rois[:, 2] - ex_rois[:, 0] + 1.0
+    ex_heights = ex_rois[:, 3] - ex_rois[:, 1] + 1.0
+    ex_ctr_x = ex_rois[:, 0] + 0.5 * ex_widths
+    ex_ctr_y = ex_rois[:, 1] + 0.5 * ex_heights
+
+    gt_widths = gt_rois[:, 2] - gt_rois[:, 0] + 1.0
+    gt_heights = gt_rois[:, 3] - gt_rois[:, 1] + 1.0
+    gt_ctr_x = gt_rois[:, 0] + 0.5 * gt_widths
+    gt_ctr_y = gt_rois[:, 1] + 0.5 * gt_heights
+
+    targets_dx = (gt_ctr_x - ex_ctr_x) / ex_widths
+    targets_dy = (gt_ctr_y - ex_ctr_y) / ex_heights
+    targets_dw = torch.log(gt_widths / ex_widths)
+    targets_dh = torch.log(gt_heights / ex_heights)
+
+    targets = torch.stack(
+        (targets_dx, targets_dy, targets_dw, targets_dh), 1)
+    return targets
+
+
+def bbox_transform_inv(boxes, deltas):
+    # Input should be both tensor or both Variable and on the same device
+    if len(boxes) == 0:
+        return deltas.detach() * 0
+
+    widths = boxes[:, 2] - boxes[:, 0] + 1.0
+    heights = boxes[:, 3] - boxes[:, 1] + 1.0
+    ctr_x = boxes[:, 0] + 0.5 * widths
+    ctr_y = boxes[:, 1] + 0.5 * heights
+
+    dx = deltas[:, 0::4]
+    dy = deltas[:, 1::4]
+    dw = deltas[:, 2::4]
+    dh = deltas[:, 3::4]
+
+    pred_ctr_x = dx * widths.unsqueeze(1) + ctr_x.unsqueeze(1)
+    pred_ctr_y = dy * heights.unsqueeze(1) + ctr_y.unsqueeze(1)
+    pred_w = torch.exp(dw) * widths.unsqueeze(1)
+    pred_h = torch.exp(dh) * heights.unsqueeze(1)
+
+    pred_boxes = torch.cat( \
+        [_.unsqueeze(2) for _ in [pred_ctr_x - 0.5 * pred_w, \
+                                  pred_ctr_y - 0.5 * pred_h, \
+                                  pred_ctr_x + 0.5 * pred_w, \
+                                  pred_ctr_y + 0.5 * pred_h]], 2).view(
+        len(boxes), -1)
+
+    return pred_boxes
+
+
+def clip_boxes(boxes, im_shape):
+    """
+    Clip boxes to image boundaries.
+    boxes must be tensor or Variable, im_shape can be anything but Variable
+    """
+
+    if not hasattr(boxes, 'data'):
+        boxes_ = boxes.numpy()
+
+    boxes = boxes.view(boxes.size(0), -1, 4)
+    boxes = torch.stack( \
+        [boxes[:, :, 0].clamp(0, im_shape[1] - 1),
+         boxes[:, :, 1].clamp(0, im_shape[0] - 1),
+         boxes[:, :, 2].clamp(0, im_shape[1] - 1),
+         boxes[:, :, 3].clamp(0, im_shape[0] - 1)], 2).view(boxes.size(0), -1)
+
+    return boxes
+
+
+def bbox_overlaps(boxes, query_boxes):
+    """
+    Parameters
+    ----------
+    boxes: (N, 4) ndarray or tensor or variable
+    query_boxes: (K, 4) ndarray or tensor or variable
+    Returns
+    -------
+    overlaps: (N, K) overlap between boxes and query_boxes
+    """
+    if isinstance(boxes, np.ndarray):
+        boxes = torch.from_numpy(boxes)
+        query_boxes = torch.from_numpy(query_boxes)
+        # If input is ndarray, turn the overlaps back to ndarray when return
+        out_fn = lambda x: x.numpy()
+    else:
+        out_fn = lambda x: x
+
+    box_areas = (boxes[:, 2] - boxes[:, 0] + 1) * \
+                (boxes[:, 3] - boxes[:, 1] + 1)
+    query_areas = (query_boxes[:, 2] - query_boxes[:, 0] + 1) * \
+                  (query_boxes[:, 3] - query_boxes[:, 1] + 1)
+
+    iw = (torch.min(boxes[:, 2:3], query_boxes[:, 2:3].t()) - torch.max(
+        boxes[:, 0:1], query_boxes[:, 0:1].t()) + 1).clamp(min=0)
+    ih = (torch.min(boxes[:, 3:4], query_boxes[:, 3:4].t()) - torch.max(
+        boxes[:, 1:2], query_boxes[:, 1:2].t()) + 1).clamp(min=0)
+    ua = box_areas.view(-1, 1) + query_areas.view(1, -1) - iw * ih
+    overlaps = iw * ih / ua
+    return out_fn(overlaps)
