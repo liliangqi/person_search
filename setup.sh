56107f3ce120f1c272c9b05e3f7c4bb7ebebfa8e
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-04-08 20:28:41 +0800

    add test conditions

--- setup.sh
+++ setup.sh
@@ -5,3 +5,10 @@ nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_61
 cd ../..
 python build.py
 cd ..
+
+# test on my own computer
+ln -s ../person_search_caffe/data/psdb data
+cd data
+mkdir pretrained
+cp /home/liliangqi/res50.pth pretrained/
+cd ..
