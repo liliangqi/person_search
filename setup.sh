f6316b32ac4751f4c93ea24f760acb3e526c698c
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-04-16 15:17:50 +0800

    fix computational ability

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # build nms
 cd nms/src/cuda
 echo "Compiling nms kernels by nvcc..."
-nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_61
+nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
 cd ../..
 python build.py
 cd ..
