e23e40e3274b85d5b98d129d17544700732a786a
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-05-11 18:03:14 +0800

    set different units

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 #
 # Author: Liangqi Li
 # Creating Date: Apr 14, 2018
-# Latest rectified: Apr 14, 2018
+# Latest rectified: May 11, 2018
 # -----------------------------------------------------
 import time
 import functools
@@ -14,7 +14,16 @@ def clock_non_return(func):
     def clocked(*args, **kwargs):
         t0 = time.time()
         func(*args, **kwargs)
-        eplapsed = time.time() - t0
+        elapsed = time.time() - t0
+        if elapsed < 60:
+            trans_elap = elapsed
+            unit = 'seconds'
+        elif elapsed < 3600:
+            trans_elap = elapsed / 60
+            unit = 'minutes'
+        else:
+            trans_elap = elapsed / 3600
+            unit = 'hours'
         print('\n' + '*' * 40)
-        print('Entire process costs {:.2f} hours.'.format(eplapsed / 3600))
+        print('Entire process costs {:.2f} {:s}.'.format(trans_elap, unit))
     return clocked
