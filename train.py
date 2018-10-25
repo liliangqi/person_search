722c4a2148d6789e272c54092686a5e2587f282f
liliangqi <Lewis_lee@sjtu.edu.cn>
2018-10-25 19:01:39 +0800

    Add TensorBoard and `lr_scheduler`

--- train.py
+++ train.py
@@ -3,20 +3,22 @@
 #
 # Author: Liangqi Li
 # Creating Date: Mar 31, 2018
-# Latest rectified: Aug 7, 2018
+# Latest rectified: Oct 25, 2018
 # -----------------------------------------------------
 import os
-import argparse
-
-import torch
-from torch.utils.data import DataLoader
-import yaml
 import time
 
-from __init__ import clock_non_return
+import yaml
+import argparse
+import torch
+from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler
+
+from utils.utils import clock_non_return, AverageMeter
+from utils.logger import TensorBoardLogger
 from dataset.sipn_dataset import SIPNDataset
 import dataset.sipn_transforms as sipn_transforms
-from model import SIPN
+from models.model import SIPN
 
 
 def parse_args():
@@ -24,10 +26,11 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description='Training')
     parser.add_argument('--net', default='res50', type=str)
-    parser.add_argument('--epochs', default=20, type=int)
+    parser.add_argument('--max_epoch', default=20, type=int)
     parser.add_argument('--gpu_ids', default='0', type=str)
     parser.add_argument('--data_dir', default='', type=str)
     parser.add_argument('--lr', default=0.0001, type=float)
+    parser.add_argument('--step_size', default=[7, 14], nargs='+', type=int)
     parser.add_argument('--optimizer', default='SGD', type=str)
     parser.add_argument('--out_dir', default='./output', type=str)
     parser.add_argument('--pre_model', default='', type=str)
@@ -39,63 +42,65 @@ def parse_args():
     return args
 
 
-def train_model(dataloader, net, lr, optimizer, num_epochs, save_dir,
-                resume_epoch):
+def train_model(dataloader, net, optimizer, epoch):
     """Train the model"""
 
-    all_epoch_loss = 0
-    dataset_len = len(dataloader.dataset)
-    start = time.time()
-    net.train()
-
+    lr = optimizer.param_groups[0]['lr']
+    end = time.time()
     with open('config.yml', 'r') as f:
         config = yaml.load(f)
 
-    for epoch in range(resume_epoch, num_epochs):
-        epoch_start = time.time()
-        if epoch in [4, 8]:
-            lr *= config['gamma']  # TODO: use lr_scheduel
-            for param_group in optimizer.param_groups:
-                param_group['lr'] *= config['gamma']
+    for iter_idx, data in enumerate(dataloader):
+        im, (gt_boxes, im_info) = data
+        im = im.to(device)
+        gt_boxes = gt_boxes.squeeze(0).to(device)
+        im_info = im_info.numpy().squeeze(0)
 
-        for step, data in enumerate(dataloader):
-            im, (gt_boxes, im_info) = data
-            im = im.to(device)
-            gt_boxes = gt_boxes.squeeze(0).to(device)
-            im_info = im_info.numpy().squeeze(0)
+        # Forward and backward
+        losses = net(im, gt_boxes, im_info)
+        optimizer.zero_grad()
+        sum_loss = sum(losses)
+        sum_loss.backward()
+        optimizer.step()
 
-            losses = net(im, gt_boxes, im_info)
-            optimizer.zero_grad()
-            total_loss = sum(losses)
-            total_loss.backward()
-            optimizer.step()
+        # Compute average loss and average time over all iterations
+        current_loss = sum_loss.item()
+        total_loss.update(current_loss)
+        time_cost.update(time.time() - end)
+        end = time.time()
 
-            all_epoch_loss += total_loss.item()
-            current_iter = epoch * dataset_len + step + 1
-            average_loss = all_epoch_loss / current_iter
+        # Show status
+        if (iter_idx + 1) % config['disp_interval'] == 0:
+            print('Epoch {:2d}, iter {:5d}, average loss: {:.6f}, lr: '
+                  '{:.2e}'.format(epoch+1, iter_idx+1, total_loss.avg, lr))
+            print('>>>> rpn_cls: {:.6f}'.format(losses[0].item()))
+            print('>>>> rpn_box: {:.6f}'.format(losses[1].item()))
+            print('>>>> cls: {:.6f}'.format(losses[2].item()))
+            print('>>>> box: {:.6f}'.format(losses[3].item()))
+            print('>>>> reid: {:.6f}'.format(losses[4].item()))
+            print('Average time: {:.3f}s/iter'.format(time_cost.avg))
 
-            torch.cuda.empty_cache()
+        step = total_loss.count
+        # TensorBoard logging
+        if step % (config['disp_interval'] * 5) == 0:
+            # Scalar values
+            info = {'total_loss': current_loss,
+                    'rpn_cls_loss': losses[0].item(),
+                    'rpn_box_loss': losses[1].item(),
+                    'cls_loss': losses[2].item(),
+                    'box_loss': losses[3].item(),
+                    'reid_loss': losses[4].item()}
+            for tag, value in info.items():
+                tensor_logger.scalar_summary(tag, value, step)
 
-            if (step+1) % config['disp_interval'] == 0:
-                end = time.time()
-                print('Epoch {:2d}, iter {:5d}, average loss: {:.6f}, lr: '
-                      '{:.2e}'.format(epoch+1, step+1, average_loss, lr))
-                print('>>>> rpn_cls: {:.6f}'.format(losses[0].item()))
-                print('>>>> rpn_box: {:.6f}'.format(losses[1].item()))
-                print('>>>> cls: {:.6f}'.format(losses[2].item()))
-                print('>>>> box: {:.6f}'.format(losses[3].item()))
-                print('>>>> reid: {:.6f}'.format(losses[4].item()))
-                print('time cost: {:.3f}s/iter'.format(
-                    (end - start) / (epoch * dataset_len + (step + 1))))
+            # Values of parameters and gradients
+            for tag, value in net.named_parameters():
+                tag = tag.replace('.', '/')
+                tensor_logger.hist_summary(tag, value.data.cpu().numpy(), step)
+                tensor_logger.hist_summary(
+                    tag + '/grad', value.grad.data.cpu().numpy(), step)
 
-        epoch_end = time.time()
-        print('\nEntire epoch time cost: {:.2f} hours\n'.format(
-            (epoch_end - epoch_start) / 3600))
-
-        # Save the trained model after each epoch
-        save_name = os.path.join(
-            save_dir, 'sipn_{}_{}.pth'.format(net.net_name, epoch + 1))
-        torch.save(net.state_dict(), save_name)
+        torch.cuda.empty_cache()
 
 
 @clock_non_return
@@ -106,7 +111,7 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     save_dir = os.path.join(opt.out_dir, opt.dataset_name)
-    print('Trained models will be save to', os.path.abspath(save_dir))
+    print('Trained models will be saved to', os.path.abspath(save_dir))
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
@@ -116,12 +121,6 @@ def main():
 
     model = SIPN(opt.net, opt.dataset_name, pre_model)
     model.to(device)
-
-    if opt.resume:
-        resume = os.path.join(save_dir, 'sipn_' + opt.net + '_' +
-                              str(opt.resume) + '.pth')
-        print('Resuming model check point from {}'.format(resume))
-        model.load_trained_model(torch.load(resume))
 
     # Read the configuration file
     with open('config.yml', 'r') as f:
@@ -155,16 +154,51 @@ def main():
     if opt.optimizer == 'SGD':
         optimizer = torch.optim.SGD(params, momentum=0.9)
     elif opt.optimizer == 'Adam':
-        # lr *= 0.1
         optimizer = torch.optim.Adam(params)
     else:
         raise KeyError(opt.optimizer)
 
-    # Train the model
-    train_model(dataloader, model, lr, optimizer, opt.epochs, save_dir,
-                opt.resume)
+    global total_loss
+    global time_cost
+    global tensor_logger
 
-    print('Done.\n')
+    start_epoch = opt.resume
+    total_loss = AverageMeter()
+    time_cost = AverageMeter()
+    tensor_logger = TensorBoardLogger('./logs/TensorBoard')
+
+    if opt.resume:
+        resume = os.path.join(save_dir, 'sipn_' + opt.net + '_' +
+                              str(opt.resume) + '.tar')
+        print('Resuming model check point from {}'.format(resume))
+        checkpoint = torch.load(resume)
+        start_epoch = checkpoint['epoch']
+        model.load_trained_model(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        total_loss = checkpoint['total_loss']
+
+    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=opt.step_size,
+                                         gamma=0.1, last_epoch=start_epoch-1)
+    # Train the model
+    for epoch in range(start_epoch, opt.max_epoch):
+        epoch_start = time.time()
+        model.train()
+
+        train_model(dataloader, model, optimizer, epoch)
+        scheduler.step()
+
+        epoch_end = time.time()
+        print('\nEntire epoch time cost: {:.2f} hours\n'.format(
+            (epoch_end - epoch_start) / 3600))
+
+        # Save the trained model after each epoch
+        save_name = os.path.join(
+            save_dir, 'sipn_{}_{}.tar'.format(model.net_name, epoch + 1))
+        checkpoint = {'epoch': epoch + 1,
+                      'model_state_dict': model.state_dict(),
+                      'optimizer_state_dict': optimizer.state_dict(),
+                      'total_loss': total_loss}
+        torch.save(checkpoint, save_name)
 
 
 if __name__ == '__main__':
