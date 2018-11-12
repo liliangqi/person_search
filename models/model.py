9b3764726dc8f053a97376345d95cf7e252bd418
liliangqi <Lewis_lee@sjtu.edu.cn>
2018-11-12 19:57:32 +0800

    Simplify the process to extract features for the triplet

--- models/model.py
+++ models/model.py
@@ -14,7 +14,7 @@ from .vgg16 import Vgg16
 from .resnet import MyResNet
 from .densenet import DenseNet
 from .strpn import STRPN
-from utils.losses import oim_loss, smooth_l1_loss, TripletLoss
+from utils.losses import smooth_l1_loss, TripletLoss
 
 
 class SIPN(nn.Module):
@@ -64,107 +64,34 @@ class SIPN(nn.Module):
         self.reid_feat_net = nn.Linear(self.fc7_channels, self.reid_feat_dim)
         self.init_linear_weight(False)
 
-        self.triplet_loss = TripletLoss()
-
     def forward(self, im_data, gt_boxes, im_info, mode='gallery'):
         if self.training:
-            # ###############################################################
-            # ========================Triplet Loss===========================
-            # ###############################################################
-            if isinstance(im_data, tuple):
-                assert isinstance(gt_boxes, tuple)
-                assert isinstance(im_info, tuple)
+            if mode == 'query':
+                assert gt_boxes.size(0) == 1
+                # im_h = im_data.size(2)
+                # im_w = im_data.size(3)
+                # x1, y1, x2, y2 = gt_boxes[0]
+                # w = x2 - x1
+                # h = y2 - y1
+                #
+                # sw = w / im_w
+                # sh = h / im_h
+                # tx = (x1 + x2) / im_w - 1
+                # ty = (y1 + y2) / im_h - 1
+                #
+                # theta = torch.Tensor([[sw, 0, tx, 0, sh, ty]]).view(-1, 2, 3)
+                # im_crop = func.grid_sample(im_data, func.affine_grid(
+                #     theta, torch.Size([1, 3, h, w])))
 
-                # Extract the feature of the query
-                q_im = im_data[0]
-                q_box = gt_boxes[0]
-                q_info = im_info[0]
-
-                q_box = torch.cat((torch.zeros(1, 1).cuda(), q_box[:, :4]), 1)
-                q_net_conv = self.head(q_im)
-                q_pool_feat = self.strpn(q_net_conv, q_box, q_info, 'query')
+                net_conv = self.head(im_data)
+                # # TODO: move pooling layer from strpn to SIPN
+                # pooled_feat = self.strpn(net_conv, gt_boxes, im_info, mode)
                 if self.net_name == 'vgg16':
-                    q_pool_feat = q_pool_feat.view(q_pool_feat.size(0), -1)
-                    q_fc7 = self.tail(q_pool_feat)
+                    fc7 = self.tail(net_conv)
                 else:
-                    q_fc7 = self.tail(q_pool_feat).mean(3).mean(2)
-                q_reid_feat = self.reid_feat_net(q_fc7)
-
-                # Extract the feature of the positive gallery
-                p_im = im_data[1]
-                p_boxes = gt_boxes[1]
-                p_info = im_info[1]
-
-                p_net_conv = self.head(p_im)
-                p_pool_feat, p_tr_feat, p_rpn_loss, p_label, p_bbox_info = \
-                    self.strpn(p_net_conv, p_boxes, p_info)
-                if self.net_name == 'vgg16':
-                    p_pool_feat = p_pool_feat.view(p_pool_feat.size(0), -1)
-                    p_fc7 = self.tail(p_pool_feat)
-                else:
-                    p_fc7 = self.tail(p_pool_feat).mean(3).mean(2)
-                p_cls_score = self.cls_score_net(p_fc7)
-                p_bbox_pred = self.bbox_pred_net(p_fc7)
-                p_reid_feat = self.reid_feat_net(p_fc7)
-
-                p_det_label, p_pid_label = p_label
-                p_det_label = p_det_label.view(-1)
-                p_cls_loss = func.cross_entropy(
-                    p_cls_score.view(-1, 2), p_det_label)
-                p_bbox_loss = smooth_l1_loss(p_bbox_pred, p_bbox_info)
-                p_rpn_cls_loss, p_rpn_box_loss = p_rpn_loss
-
-                # Extract the feature of the negative gallery
-                n_im = im_data[2]
-                n_boxes = gt_boxes[2]
-                n_info = im_info[2]
-
-                n_net_conv = self.head(n_im)
-                n_pool_feat, n_tr_feat, n_rpn_loss, n_label, n_bbox_info = \
-                    self.strpn(n_net_conv, n_boxes, n_info)
-                if self.net_name == 'vgg16':
-                    n_pool_feat = n_pool_feat.view(n_pool_feat.size(0), -1)
-                    n_fc7 = self.tail(n_pool_feat)
-                else:
-                    n_fc7 = self.tail(n_pool_feat).mean(3).mean(2)
-                n_cls_score = self.cls_score_net(n_fc7)
-                n_bbox_pred = self.bbox_pred_net(n_fc7)
-                n_reid_feat = self.reid_feat_net(n_fc7)
-
-                n_det_label, n_pid_label = n_label
-                n_det_label = n_det_label.view(-1)
-                n_cls_loss = func.cross_entropy(
-                    n_cls_score.view(-1, 2), n_det_label)
-                n_bbox_loss = smooth_l1_loss(n_bbox_pred, n_bbox_info)
-                n_rpn_cls_loss, n_rpn_box_loss = n_rpn_loss
-
-                # Compute loss
-                rpn_cls_loss = p_rpn_cls_loss + n_rpn_cls_loss
-                rpn_box_loss = p_rpn_box_loss + n_rpn_box_loss
-                cls_loss = p_cls_loss + n_cls_loss
-                bbox_loss = p_bbox_loss + n_bbox_loss
-
-                query_pid = int(gt_boxes[0][:, -1].item())
-                p_mask = (p_pid_label.squeeze() != self.num_pid).nonzero(
-                    ).squeeze().view(-1)
-                p_pid_label_drop = p_pid_label[p_mask]
-                p_reid_feat_drop = p_reid_feat[p_mask]
-                n_mask = (n_pid_label.squeeze() != self.num_pid).nonzero(
-                    ).squeeze().view(-1)
-                n_pid_label_drop = n_pid_label[n_mask]
-                n_reid_feat_drop = n_reid_feat[n_mask]
-
-                tri_label = torch.cat(
-                    (p_pid_label_drop, n_pid_label_drop)).squeeze()
-                tri_feat = torch.cat((p_reid_feat_drop, n_reid_feat_drop), 0)
-                reid_loss = self.triplet_loss(
-                    q_reid_feat, query_pid, tri_feat, tri_label, mode='hard')
-
-                return rpn_cls_loss, rpn_box_loss, cls_loss, bbox_loss,\
-                    reid_loss
-
-            # ###############################################################
-            # ###############################################################
+                    fc7 = self.tail(net_conv).mean(3).mean(2)
+                reid_feat = self.reid_feat_net(fc7)
+                return reid_feat
 
             net_conv = self.head(im_data)
             # Returned parameters contain 3 tuples here
@@ -180,17 +107,16 @@ class SIPN(nn.Module):
 
             # reid_fc7 = self.tail(trans_feat).mean(3).mean(2)
             # reid_feat = F.normalize(self.reid_feat_net(reid_fc7))
-            reid_feat = func.normalize(self.reid_feat_net(fc7))
+            reid_feat = self.reid_feat_net(fc7)
 
             det_label, pid_label = label
             det_label = det_label.view(-1)
             cls_loss = func.cross_entropy(cls_score.view(-1, 2), det_label)
             bbox_loss = smooth_l1_loss(bbox_pred, bbox_info)
-            reid_loss = oim_loss(reid_feat, pid_label, self.lut, self.queue,
-                                 gt_boxes.size(0), self.lut_momentum)
             rpn_cls_loss, rpn_box_loss = rpn_loss
+            det_loss = rpn_cls_loss, rpn_box_loss, cls_loss, bbox_loss
 
-            return rpn_cls_loss, rpn_box_loss, cls_loss, bbox_loss, reid_loss
+            return det_loss, reid_feat, pid_label
 
         else:
             if mode == 'gallery':
